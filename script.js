2a036dec02f3a8bc9e8a17a266627e3a5d8a94a1
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-11-13 23:33:09 +0600

    Update BMI Calculator Code

--- script.js
+++ script.js
@@ -9,7 +9,19 @@ button.addEventListener("click", function () {
   let newHeight = parseFloat(height.value);
   let newWeight = parseFloat(weight.value);
   newHeight = newHeight / 100;
-  let sqrHeight = newHeight * newHeight
+  let sqrHeight = newHeight * newHeight;
+
+  let bmi = newWeight / sqrHeight;
+  score.textContent = bmi.toFixed(2);
+  result.style.display = "block";
+
+  if (score.textContent < 18.6) {
+     score.style.background = "#ffeaa7";
+  } else if (score.textContent < 24.9) {
+     score.style.background = "#55efc4";
+  } else {
+    score.style.background = "#d63031";
+  }
 });
 
 let form = document.getElementById("form");
