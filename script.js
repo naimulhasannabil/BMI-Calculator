cc8fed7c5afc103f716bb75b36c213aae7c98457
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-11-13 22:03:47 +0600

    Update BMI Calculator Project Code

---
+++ script.js
@@ -0,0 +1,6 @@
+let height = document.getElementById("height");
+let weight = document.getElementById("Weight");
+let button = document.getElementById("button");
+
+let score = document.getElementById("score");
+let result = document.querySelector(".result");
