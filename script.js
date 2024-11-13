47b7a995224a358aa18fed6da7f055c7db06bc76
Naimul Hasan Nabil <naimulhasannabil@gmail.com>
2024-11-14 01:01:41 +0600

    Modify the project

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let height = document.getElementById("height");
+let height2 = document.getElementById("height2");
 let weight = document.getElementById("Weight");
 let button = document.getElementById("button");
 
@@ -7,24 +8,30 @@ let result = document.querySelector(".result");
 
 button.addEventListener("click", function () {
   let newHeight = parseFloat(height.value);
+  let newHeight2 = parseFloat(height2.value);
   let newWeight = parseFloat(weight.value);
-  newHeight = newHeight / 100;
-  let sqrHeight = newHeight * newHeight;
+  let foot = newHeight * 0.3048;
+
+  let inch = newHeight2 * 0.0254;
+
+  let meter = foot + inch;
+
+  let sqrHeight = (meter * meter).toFixed(8);
 
   let bmi = newWeight / sqrHeight;
   score.textContent = bmi.toFixed(2);
   result.style.display = "block";
 
   if (score.textContent < 18.6) {
-     score.style.background = "#ffeaa7";
+    score.style.background = "#ffeaa7";
   } else if (score.textContent < 24.9) {
-     score.style.background = "#55efc4";
+    score.style.background = "#55efc4";
   } else {
     score.style.background = "#d63031";
   }
 });
 
 let form = document.getElementById("form");
-form.addEventListener('submit', function(e){
+form.addEventListener("submit", function (e) {
   e.preventDefault();
-})
+});
